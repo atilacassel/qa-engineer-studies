779a45c10dac563f66ae2e90c953d77b14e1efc0
Atila <atilacassel@gmail.com>
2023-07-11 15:13:16 +0100

    estudos variaveis

---
+++ javascript/variaveis.js
@@ -0,0 +1,44 @@
+//// ===> VAR
+/* var nome = 'Átila';
+console.log('Meu nome é', nome); */
+
+/* function imprimirNome() {
+    var nome = 'Atila Cassel'
+    console.log('Meu nome é', nome);
+}
+
+//console.log('Meu nome é, nome); *Aqui a variavel nao e visivel*
+
+imprimirNome(); */
+
+//// ===> LET
+/* function imprimirIdade() {
+    for (let idade = 37; idade <= 50; idade++) {
+        console.log('Idade dentro do for: ', idade)
+    }
+    //console.log('Idade fora do for: ', idade);
+
+}
+
+imprimirIdade(); */
+
+//// ===> CONST
+/* function imprimirIdade2() {
+    for (const idade = 37; idade <= 50; idade++) {
+        console.log('Idade dentro do for: ', idade)
+    }
+}
+
+imprimirIdade2(); */
+// Não funciona pois const não pode ser alterado.
+
+const pessoa = {
+    nome: 'Atila',
+    idade: 37
+}
+
+function imprimirDadosPessoa() {
+    console.log(`Nome é: ${pessoa.nome} e a idade é: ${pessoa.idade}`)
+}
+
+imprimirDadosPessoa();
