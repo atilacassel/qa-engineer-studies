8dbe2d0b6e35b235dc177e0b447e54c85c93608d
Atila <atilacassel@gmail.com>
2023-08-17 18:15:54 +0100

    start jest studies

---
+++ jest/multiply.test.js
@@ -0,0 +1,17 @@
+const multiply = require("./multiply");
+
+describe("test multiply positive scenarios", () => {
+  test("multiply 3*2 should equal 6", () => {
+    expect(multiply(3, 2)).toBe(6);
+  });
+});
+
+test("object example", () => {
+  const data = { first: 1 };
+  data["second"] = 2;
+  expect(data).toEqual({ first: 1, second: 2 });
+});
+
+test("multiply 4*3 should not equal to 11", () => {
+  expect(multiply(4, 3)).not.toBe(11);
+});
