4b50efa17b9eb74697acc6be194e32548837170f
Átila Cassel <atilacassel@MacBook-Air.Home>
2023-09-15 11:59:48 +0100

    Destructuring arrays file refactor

---
+++ javascript/studies_topics/destructuringArrays.js
@@ -0,0 +1,7 @@
+/* Destructuring allows you to extract values from arrays and assign them to variables in a concise way.
+Use square brackets [] to destructure an array. */
+
+const colors = ["red", "green", "blue"];
+const [firstColor, secondColor, thirdColor] = colors;
+
+// Now, firstColor = 'red', secondColor = 'green', thirdColor = 'blue'
