3777874cf0e764dceac4e0403623159db16ec66c
Átila Cassel <atilacassel@MacBook-Air.Home>
2023-09-15 11:48:23 +0100

    Arrow functions file refactor

--- javascript/studies_topics/arrow_functions.js
+++ javascript/studies_topics/arrow_functions.js
@@ -1,14 +1,26 @@
-/* const numeros = [1, 2, 3, 4, 5];
-const quadrados = numeros.map((numero) => numero * numero);
-console.log(quadrados); */
+/* Arrow functions are a shorter way to write functions in JavaScript.
+They have a concise syntax using the => (fat arrow) operator.
+Arrow functions inherit the this value from their surrounding context. */
 
-/* Função map() é usada para criar uma nova array com base 
-em outra array, aplicando uma transformação a cada elemento. */
+// Regular function
+function greet(name) {
+    return "Hello, " + name + "!";
+}
 
-/* const nomes = ["Ana", "João", "Maria", "Pedro"];
-const nomesComSobrenome = nomes.map((nome) => nome + " Silva");
-console.log(nomesComSobrenome); */
+// Arrow function
+const greetArrow = (name) => "Hello, " + name + "!";
+
+
+/* The map() method is useful when we want to transform each element
+of an array into another value, while the filter() method is useful
+for filtering elements based on a specific condition.
+ */
+
+const numbers = [1, 2, 3];
+const doubled = numbers.map(number => number * 2);
+// Output: [2, 4, 6]
 
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = numeros.filter((numero) => numero % 2 === 0);
 console.log(pares);
+// Output: [2, 4]
